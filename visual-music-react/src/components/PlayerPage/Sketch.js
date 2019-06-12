ae917fd3c30c3caab370505f11cb8d1522f4c390
YogiHa <hagaiharari1@gmail.com>
2019-06-12 23:35:52 +0300

    fixing array length while resize for small window width bug

--- visual-music-react/src/components/PlayerPage/Sketch.js
+++ visual-music-react/src/components/PlayerPage/Sketch.js
@@ -4,9 +4,9 @@ import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
 import * as p5 from 'p5';
 
-
+// the sketch getting argument p stand for 'prototype' - for more info check react-p5-wrapper npm package
 export default function sketch(p) {
-// this example made just for the begging and to get used and famalliar with p5 syntax
+// this example made just for the begging and to get used and famalliar with p5 syntax, and syntax adjust for react
 
 // creating emptey array, each milisecond in the audio should send arguement to here 
 let MiliSecondsArray = [];
@@ -16,6 +16,7 @@ let sliderGreen;
 let sliderBlue;
 let button;
 let amp;
+
 p.preload = function() {
   song = p.loadSound(octuopus);
 }
@@ -30,6 +31,7 @@ function toggleSong() {
     song.play()
   }
 }
+        // this awfull name its from according to the docs :/
  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
     if (props.isFullSize){
  p.resizeCanvas(p.windowWidth, p.windowHeight); 
@@ -39,8 +41,11 @@ function toggleSong() {
   };
 
   p.windowResized = function (props) {
+    if (MiliSecondsArray.length > p.windowWidth -70 ) {
+      MiliSecondsArray.splice(0 ,70 + MiliSecondsArray.length - p.windowWidth)
+    }
     if (props.isFullSize){
-  p.resizeCanvas(p.windowWidth, p.windowHeight-20); 
+  p.resizeCanvas(p.windowWidth, p.windowHeight); 
     } else {
       p.resizeCanvas(p.windowWidth,400)
     }
@@ -48,7 +53,6 @@ function toggleSong() {
 
  p.setup = function() {
    p.createCanvas(p.windowWidth, 400);
-           // creating sliders, first position-arg for width second for p.height
    sliderRed = p.createSlider(0, 255, 100);
    sliderGreen = p.createSlider(0, 255, 0);
    sliderBlue = p.createSlider(0, 255, 255);
@@ -58,7 +62,6 @@ function toggleSong() {
    //in this example the data that insert to the array its the amp, not nessecary!
     amp = new p5.Amplitude()
 }
-
 
  p.draw = function() {
          // setting dinamic background when music played, i know its very basic, but its for start :) (using UnSplash API?)
@@ -75,7 +78,7 @@ function toggleSong() {
     p.noFill();
     p.beginShape(); 
     for (let i = 0; i< MiliSecondsArray.length; i++) {
-        let y = p.map(MiliSecondsArray[i], 0, 1, p.height / 2, 0)
+        let y = p.map(MiliSecondsArray[i], 0, 1, p.height / 4, 0)
         p.vertex(i, y);
     }
     p.endShape();
@@ -83,7 +86,7 @@ function toggleSong() {
     p.noFill();
     p.beginShape(); 
     for (let i = 0; i< MiliSecondsArray.length; i++) {
-        let y = p.map(MiliSecondsArray[i], 0, 1, p.height / 4, 0)
+        let y = p.map(MiliSecondsArray[i], 0, 1, p.height / 2, 0)
         p.vertex(i, y);
     }
     p.endShape();
@@ -95,13 +98,12 @@ function toggleSong() {
         p.vertex(i, y);
     }
     p.endShape();
-        // the margin-end of the blue line is 50
-    if (MiliSecondsArray.length > p.width - 50) {
-      MiliSecondsArray.splice(0,1)
+        // the margin-end of the blue line is 70
+    if (MiliSecondsArray.length > p.windowWidth - 70) {
+      MiliSecondsArray.splice(0 ,1)
     }
     p.stroke(0, 255 ,255);
     p.line(MiliSecondsArray.length, 0, MiliSecondsArray.length, p.height)
   } 
 }
-
 }
