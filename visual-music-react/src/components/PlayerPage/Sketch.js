94ddb281af136bcf8a4625196e155a6b363ed5de
YogiHa <hagaiharari1@gmail.com>
2019-06-14 01:50:32 +0300

    two songs,both outside sketch

--- visual-music-react/src/components/PlayerPage/Sketch.js
+++ visual-music-react/src/components/PlayerPage/Sketch.js
@@ -1,15 +1,13 @@
 // this canvas its only an example! some of the elemets here will probably stay and more important, like the
 // full-size functionality or the point of pushing every miliSecond arg to an array,
 // but the style, the shape of the draw etc are just v0.0.1 ...
-import React from 'react'
-import octuopus from './OctopusGarden.mp3'
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
 import * as p5 from 'p5';
 
 // where is thie 'p' coming from? thank to react-p5-wrapper package we able to use p5.js syntax such preload or createCanvas, with tha addition of p.
 export default function sketch(p) {
-
+let shouldIPlay = false;
 // creating emptey array, each milisecond in the audio should send arguement to here 
 let MiliSecondsArray = [];
 let song;
@@ -18,32 +16,35 @@ let sliderGreen;
 let sliderBlue;
 let amp;
 
-p.preload = function() {
-  song = p.loadSound(octuopus);
+const setShouldIPlay =() => {
+  shouldIPlay = true
+
    }    
   // those functions are for rerender as the things happen in the app, the setup and draw vanila p5.js functions are next               
                  // this awfull name its according to react-p5-wrapper docs :/
                p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-                {props.isFullSize ?   p.resizeCanvas(p.windowWidth, p.windowHeight) :  p.resizeCanvas(p.windowWidth,400)}
-                   if (!props.isSongStop && !props.isSongPause && !song.isPlaying()) {
-                    song.play()
-                  } else if (!props.isSongStop && props.isSongPause) {
-                    song.pause()
-                  } else if (props.isSongStop) {
-                    song.stop();
-                    MiliSecondsArray= []
-                  } }
+                {props.isFullSize ?   p.resizeCanvas(p.windowWidth, p.windowHeight) :  p.resizeCanvas(p.windowWidth,400)};
+                 if (shouldIPlay){
+                    if (!props.isSongStop && !props.isSongPause && !song.isPlaying()) {
+                     song.play()
+                   } else if (!props.isSongStop && props.isSongPause) {
+                     song.pause()
+                   } else if (props.isSongStop) {
+                     song.stop();
+                     MiliSecondsArray= []
+                              }}
+                   if (props.isNewSong) {
+                    song = p.loadSound(props.song);
+                    setShouldIPlay()  }
+                   }
                  
 
                 p.windowResized = function (props) {
                   if (MiliSecondsArray.length > p.windowWidth -70 ) {
                     MiliSecondsArray.splice(0 ,70 + MiliSecondsArray.length - p.windowWidth)
                   }
-                  if (props.isFullSize){
-                p.resizeCanvas(p.windowWidth, p.windowHeight); 
-                  } else {
-                    p.resizeCanvas(p.windowWidth,400)
-                  }
+                   p.resizeCanvas(p.windowWidth,p.height)
+
                 };
 
 
@@ -63,6 +64,7 @@ p.preload = function() {
     const b = sliderBlue.value();
     p.background(r, g, b);
 
+  if (shouldIPlay){
   if (song.isPlaying()) {
           // the 3 lines, check y p.height 
      let vol = amp.getLevel();
@@ -97,6 +99,6 @@ p.preload = function() {
     }
     p.stroke(0, 255 ,255);
     p.line(MiliSecondsArray.length, 0, MiliSecondsArray.length, p.height)
-  } 
+  } }
 }
 }
